56b658a26bf2f4c1d55ac0920dce534ace35be6a
Zhongjie268 <441252827@qq.com>
2026-09-19 13:38:48 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -4,4 +4,3 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall2.git package/openwrt-passwall2
